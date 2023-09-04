c63e9edf5f699752ce0597b000f7469514a38270
TamaraDimov <tamemojsoska@gmail.com>
2023-09-04 14:46:15 +0200

    fix rubocop

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,10 @@
 class RecipesController < ApplicationController
   def index
     @recipes = Recipe.where(user_id: current_user.id)
-   end
+  end
 
-   def show
+  def show
     @user = current_user
     @recipe = Recipe.find(params[:id])
-    end
-
+  end
 end
