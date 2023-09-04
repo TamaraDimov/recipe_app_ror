b90145c477a50432561189ecaca801ea8a7dc481
TamaraDimov <tamemojsoska@gmail.com>
2023-09-04 14:41:14 +0200

    create recipe list

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,11 @@
 class RecipesController < ApplicationController
+  def index
+    @recipes = Recipe.where(user_id: current_user.id)
+   end
+
+   def show
+    @user = current_user
+    @recipe = Recipe.find(params[:id])
+    end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+User.destroy_all
+
+User.create([{ name: "Tamara", email: "tamemojsoska@gmail.com", password: "123456" }]);
+
+Recipe.create([
+  {
+    name: "Pizza",
+    preparation_time_minutes: "40 mins",
+    cooking_time_minutes: "15mins",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce faucibus urna vitae eros ultricies aliquam. Nulla vestibulum vestibulum mi, in commodo urna malesuada ac. Proin nec diam faucibus, tempor lectus vel, luctus enim. Sed aliquet massa et metus varius, in euismod lorem faucibus. Integer sed aliquam sapien.",
+    is_public: true,
+    user_id: 9
+  },
+  {
+    name: "Lasagna",
+    preparation_time_minutes: "25 mins",
+    cooking_time_minutes: "40mins",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce faucibus urna vitae eros ultricies aliquam. Nulla vestibulum vestibulum mi, in commodo urna malesuada ac. Proin nec diam faucibus, tempor lectus vel, luctus enim. Sed aliquet massa et metus varius, in euismod lorem faucibus. Integer sed aliquam sapien.",
+    is_public: true,
+    user_id: 9
+  }
+ ])
