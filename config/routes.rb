c9d6bb9e5d0f8f7512e005e6dbd605cb1d5dc6a6
TamaraDimov <tamemojsoska@gmail.com>
2023-09-04 09:56:10 +0200

    startup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  } 
+  # resources :users, only: [:index]
+   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "welcome#index"
   # Defines the root path route ("/")
   # root "articles#index"
 end
