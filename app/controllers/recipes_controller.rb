d241b541983f25f4bf02580d0524bc73d0130be1
TamaraDimov <tamemojsoska@gmail.com>
2023-09-05 01:31:15 +0200

    fix rubocop

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,33 +7,33 @@ class RecipesController < ApplicationController
     @recipe = Recipe.new
   end
 
-  
+
   # def show
   #   @user = current_user
   #   @recipe = Recipe.find(params[:id])
   # end
-  
+
   def create
     @user = current_user
     @recipe = @user.recipes.new(recipe_params)
-    
+
     if @recipe.save
       redirect_to recipe_path(@recipe), notice: 'The recipe was succesfuly created'
     else
       render :new, alert: 'The recipe was not created'
     end
-  end 
-  
+  end
+
   def destroy
     @recipe = Recipe.find(params[:id])
     @recipe.destroy
     redirect_to recipe_path, notice: 'The recipe was deleted'
   end
-  
+
   def public
     @recipes = Recipe.where(is_public: true)
-    render 'public' 
-    end
+    render 'public'
+  end
 
   def recipe_params
     params.require(:recipe).permit(:name, :preparation_time_minutes, :cooking_time_minutes, :description, :is_public)
