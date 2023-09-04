a6ba403d5ecf9c468ab0ff1eb6d3da2cd86cc5ed
Salimer <52242629+Salimer@users.noreply.github.com>
2023-09-04 16:32:56 +0300

    Revert "Merge branch 'feature/foods-list' of https://github.com/TamaraDimov/recipe_app_ror into feature/foods-list"
    This reverts commit db4bb1fb538d07d4dd0f078354a09de6783d1529, reversing
    changes made to a5c06426fe1a4ffb46bd13c50fe836acacacb50e.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,16 +1,2 @@
 class RecipesController < ApplicationController
-  def index
-    @recipes = Recipe.where(user_id: current_user.id)
-  end
-
-  def show
-    @user = current_user
-    @recipe = Recipe.find(params[:id])
-  end
-
-  def destroy
-    @recipe = Recipe.find(params[:id])
-    @recipe.destroy
-    redirect_to recipe_path, notice: 'The recipe was deleted'
-  end
 end
