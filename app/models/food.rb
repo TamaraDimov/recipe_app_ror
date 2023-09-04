1ff9203db398580db5ef01c552d0f46dd395347f
Salimer <52242629+Salimer@users.noreply.github.com>
2023-09-04 12:58:22 +0300

    fix linters

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,17 +1,17 @@
 class Food < ApplicationRecord
-    # Associations
-    belongs_to :user, class_name: 'User'
-    has_many :recipe_foods, class_name: 'RecipeFood'
+  # Associations
+  belongs_to :user, class_name: 'User'
+  has_many :recipe_foods, class_name: 'RecipeFood'
 
-    # Attributes
-    attribute :name, :string
-    attribute :measurement_unit, :string
-    attribute :price, :decimal
-    attribute :quantity, :integer
+  # Attributes
+  attribute :name, :string
+  attribute :measurement_unit, :string
+  attribute :price, :decimal
+  attribute :quantity, :integer
 
-    # Callbacks
+  # Callbacks
 
-    # Validations
+  # Validations
 
-    # Mehtods
+  # Mehtods
 end
