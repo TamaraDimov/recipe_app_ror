48ee6d7e134e5c8a6537bff75551b04a4d537466
Salimer <52242629+Salimer@users.noreply.github.com>
2023-09-04 16:24:00 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   } 
-  resources :foods, only: [:index]
-  resources :recipes, only: [:index, :show]
+  resources :foods, only: [:index, :new, :destroy, :show]
+  resources :recipes, only: [:index, :show, :destroy]
    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "welcome#index"
   # Defines the root path route ("/")
