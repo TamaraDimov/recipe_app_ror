51d01a21cbc35d3da167dedb96cac24af4cfdbbb
Salimer <52242629+Salimer@users.noreply.github.com>
2023-09-04 12:48:29 +0300

    Update 20230904081651_create_recipes_foods.rb

--- db/migrate/20230904081651_create_recipes_foods.rb
+++ db/migrate/20230904081651_create_recipes_foods.rb
@@ -2,8 +2,9 @@ class CreateRecipesFoods < ActiveRecord::Migration[7.0]
   def change
     create_table :recipes_foods do |t|
       t.integer :quantity
-      t.integer :food_id, null: false, foreign_key: { to_table: :foods }
-      t.integer :recipe_id, null: false, foreign_key: { to_table: :recipes }
+      t.references :food, foreign_key: { to_table: :foods }
+      t.references :recipe, foreign_key: { to_table: :recipes }
+      
       t.timestamps
     end
   end
