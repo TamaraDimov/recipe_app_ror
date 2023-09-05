09a58c5bf45a3f75168476692d31ea3bb427129e
Salimer <52242629+Salimer@users.noreply.github.com>
2023-09-05 22:44:31 +0300

    Create food_spec.rb

---
+++ spec/models/food_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe Food, type: :model do
+  subject { FactoryBot.create(:food) }
+
+  before { subject.save }
+ 
+  it 'should have a name' do
+    subject.name = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'should have a valid price' do
+    subject.price = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'should have a valid quantity' do
+    subject.quantity = nil
+    expect(subject).not_to be_valid
+  end
+end
