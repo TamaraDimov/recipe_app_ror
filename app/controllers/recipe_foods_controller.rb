780001c752c028d336da079daa869e3bc998c4df
Salimer <52242629+Salimer@users.noreply.github.com>
2023-09-06 16:28:52 +0300

    Update recipe_foods_controller.rb

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,25 +1,30 @@
 class RecipeFoodsController < ApplicationController
   def new
     @user = current_user
+    @foods = @user.foods
     @recipe = Recipe.find(params[:recipe_id])
     @recipe_food = RecipeFood.new
+    @ingredients = []
+    @foods.each do |food|
+      @ingredients << food.name + " in " + food.measurement_unit
+    end
   end
 
   def create
-    @recipe = Recipe.find(params[:recipe_id])
-    @food = Food.find_by(name: params[:recipe_food][:food])
-    @quantity = params[:recipe_food][:quantity]
+    recipe = Recipe.find(params[:recipe_id])
+    food_unit = params[:recipe_food][:food]
+    quantity = params[:recipe_food][:quantity]
+    food_name = food_unit.split.first
+    food_old = Food.find_by(name: food_name)
+    food = food_old
+    food.user = nil
+    food.quantity = quantity
 
-    if @food.nil?
-      # Food doesn't exist, create a new Food record
-      @food = Food.create(name: params[:recipe_food][:food])
-    end
+    recipe_food = RecipeFood.new(recipe: recipe, food: food, quantity: quantity)
 
-    @recipe_food = RecipeFood.new(recipe_id: @recipe.id, food_id: @food.id, quantity: @quantity)
-
-    if @recipe_food.save
+    if recipe_food.save
       flash[:success] = 'Object successfully created'
-      redirect_to recipe_path(@recipe)
+      redirect_to recipe_path(recipe)
     else
       flash[:error] = 'Failed to create the recipe food'
       render 'new'
