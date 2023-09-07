abbd320b9ce73e9d646debdbdff101d0cb74f094
Salimer <52242629+Salimer@users.noreply.github.com>
2023-09-07 13:02:29 +0300

    Update recipe_food.rb

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -1,7 +1,7 @@
 class RecipeFood < ApplicationRecord
   # Associations
-  belongs_to :food, class_name: 'Food', foreign_key: 'recipe_id'
-  belongs_to :recipe, class_name: 'Recipe', foreign_key: 'food_id'
+  belongs_to :food, class_name: 'Food', foreign_key: 'food_id'
+  belongs_to :recipe, class_name: 'Recipe', foreign_key: 'recipe_id'
 
   # Attributes
   attribute :quantity, :integer
